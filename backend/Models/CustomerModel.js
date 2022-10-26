2c99d07d42666a4fdbf7b5f150b637f279507f8a
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2022-10-26 18:07:59 +0530

    customer model update

--- backend/Models/CustomerModel.js
+++ backend/Models/CustomerModel.js
@@ -3,7 +3,8 @@ const mongoose=require("mongoose")
 const CustomerSchema= new mongoose.Schema({
     username:{type:String, required:true},
     email:{type:String, required:true},
-    password:{type:String, required:true}
+    password:{type:String, required:true},
+    isAdmin: { type: Boolean, default: false}
 })
 
 const CustomerModel=mongoose.model("admin",CustomerSchema)
