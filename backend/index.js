0509f8e24f345eb4f8b21f7e846e6ecc259659dd
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2022-10-26 17:51:45 +0530

    home route

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const cors=require("cors");
+require("dotenv").config();
+
+const app=express();
+app.use(express.json());
+app.use(cors());
+
+const connection=require("./Config/config");
+
+app.get("/",(req,res)=>{
+    return res.status(200).send("HomePage")
+})
+
+app.listen(process.env.PORT,async()=>{
+    try{
+        await connection
+        console.log("DB Connected")
+    }
+    catch(err){
+        console.log(err)
+
+    }
+    console.log(`DB Connected at port ${process.env.PORT}`)
+})
+
